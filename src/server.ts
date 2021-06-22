60b399e50bd2d9c00520253c2205d63536fc984a
Anderson <andersoncleyson@protonmail.com>
2021-06-22 00:42:35 -0300

    aula02

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,7 @@
 import express from 'express';
+import "reflect-metadata";
+
+import "./database";
 
 const app = express();
 
@@ -10,15 +13,18 @@ const app = express();
  * PATCH => Alterar uma informação específica
  */
 
-
-app.get("/test", (request, response) => {
-    // Request => Entrando
-    // Response => Saindo
-    return response.send("Hello, friend")
-});
-
-app.post("/test-post", (request, response) => {
-    return response.send("Hello world")
-})
+/**
+ * Tipos de parâmetros
+ * Routes Params => http:/localhost:3000/produtos/423423423
+ * Query Parmas => http://localhost:3000/produtos?name=teclado&description=tecladobom
+ * 
+ * Body Params => {
+ *  "name": "teclado",
+ * "description": "teclado bom"
+ * 
+ * }
+ */
 
 app.listen(3000, () => console.log("Server is running..."))
+
+//50:18 - Entidades
