624bad3f0000b916a2b0048b0eb3d6039ac0d655
Anderson <andersoncleyson@protonmail.com>
2021-06-22 09:50:14 -0300

    entitiesm, repository, services, controllers

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,38 @@
+import { UsersRepositories } from "../repositories/UsersRepositories"
+import { getCustomRepository} from "typeorm";
+
+interface IUserRequest {
+    name: string;
+    email: string;
+    admin?: boolean
+}
+
+class CreateUserService {
+    async execute({name, email, admin}: IUserRequest){
+        const usersRepository = getCustomRepository(UsersRepositories);
+
+        if(!email){
+            throw new Error("Email incorrect");
+        }
+
+        const usersAlreadyExists = await usersRepository.findOne({
+            email
+        });
+
+        if (usersAlreadyExists) {
+            throw new Error("User already exists")
+        }
+
+        const user = usersRepository.create({
+            name,
+            email,
+            admin,
+        });
+
+        await usersRepository.save(user);
+
+        return user;
+    }
+}
+
+export {CreateUserService}
